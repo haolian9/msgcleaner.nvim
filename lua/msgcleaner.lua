fb9cd6365628edf37460d2737ee5a17eba038abe
haoliang <haoliang0.1.2@gmail.com>
2024-07-30 21:21:51 +0800

    sync with upstream

--- lua/msgcleaner.lua
+++ lua/msgcleaner.lua
@@ -6,8 +6,13 @@ local iuv = require("infra.iuv")
 local ni = require("infra.ni")
 local strlib = require("infra.strlib")
 
-local aug = nil ---@type infra.Augroup?
+local facts = {
+  expire = 5, --seconds
+  interval = 1000 * 1, --milliseconds
+}
+
 local timer = iuv.new_timer() ---@type uv_timer_t
+local aug = nil ---@type infra.Augroup?
 
 function M.activate()
   if aug then return end
@@ -16,24 +21,24 @@ function M.activate()
   aug = augroups.Augroup("msgcleaner://")
   aug:repeats("CmdlineLeave", { callback = function() last_cmd = os.time() end })
 
-  local last_clear = 0
+  local last_clean = 0
 
   local function clean()
     if strlib.startswith(ni.get_mode().mode, "c") then return end
     local last_msg = cthulhu.nvim.last_msg_time()
 
     local last_activity = math.max(last_msg, last_cmd)
-    if last_activity <= last_clear then return end
+    if last_activity <= last_clean then return end
 
     local now = os.time()
-    if now - last_activity < 5 then return end
+    if now - last_activity < facts.expire then return end
 
-    last_clear = last_activity
+    last_clean = last_activity
     ni.echo({ { "" } }, false, { verbose = false })
   end
 
   timer:stop()
-  timer:start(1000, 1000, vim.schedule_wrap(clean))
+  timer:start(facts.interval, facts.interval, vim.schedule_wrap(clean))
 end
 
 function M.deactivate()
